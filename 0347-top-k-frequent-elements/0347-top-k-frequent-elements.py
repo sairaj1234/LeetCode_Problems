d83afaaf40fa6b893b46b1588e766c51ba3e26c5
sairaj1234 <81349292+sairaj1234@users.noreply.github.com>
2024-01-25 00:17:00 -0600

    Time: 86 ms (86.89%), Space: 20.9 MB (94.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,10 +1,10 @@
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        Count_numbers=Counter(nums)
+        Count_numbers=Counter(nums)#counter which count the number of each char store #in the form dict
         l=[]
-        for key,value in Count_numbers.most_common(k):
+        for key,value in Count_numbers.most_common(k):#mostcommon is used for sort the values and if given any value in it like here we given k so the number of values will return 
             l.append(key)
         return l
                 
-        
+         
